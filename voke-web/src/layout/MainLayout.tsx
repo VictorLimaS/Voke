cfe27b9c2da1d733cb0dbbece4d7fa1ad28cedd0
Victor Lima <victorlimasantos99@hotmail.com>
2026-03-08 00:07:51 -0300

    style(layout): ajustar estrutura do MainLayout

--- voke-web/src/layout/MainLayout.tsx
+++ voke-web/src/layout/MainLayout.tsx
@@ -1,5 +1,7 @@
 import { Layout } from "antd"
 import HeaderBar from "../components/Header/HeaderBar"
+import CartDrawer from "../components/CartDrawer/CartDrawer"
+import { useState } from "react"
 
 const { Content, Footer } = Layout
 
@@ -8,9 +10,13 @@ interface Props {
 }
 
 export default function MainLayout({ children }: Props) {
+  const [cartOpen, setCartOpen] = useState(false)
+
   return (
     <Layout>
-      <HeaderBar />
+      <HeaderBar onOpenCart={() => setCartOpen(true)} />
+
+      <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} />
 
       <Content style={{ padding: "40px" }}>
         {children}
