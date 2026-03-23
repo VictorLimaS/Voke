276262e24a771b39643b61a652d4388646ddef35
Victor Lima <victorlimasantos99@hotmail.com>
2026-03-23 12:55:21 -0300

    refactor: update antd deprecated props to new API (Card, Drawer, Collapse, Space)

--- voke-web/src/router/routes.tsx
+++ voke-web/src/router/routes.tsx
@@ -1,62 +1,65 @@
 import { Routes, Route } from "react-router-dom"
-
-import Home from "../pages/Home/Home"
-import ProductPage from "../pages/Product/ProductPage"
-import LoginPage from "../pages/LoginPage/LoginPage"
-import CartPage from "../pages/CartPage/CartPage"
-import CheckoutPage from "../pages/Checkout/CheckoutPage"
-import ProfilePage from "../pages/Profile/ProfilePage"
+import { lazy, Suspense } from "react"
 
 import PublicRoute from "../router/PublicRoute"
 import CheckoutRoute from "../router/CheckoutRoute"
 import ProtectedRoute from "../router/ProtectedRoute"
 
+const Home = lazy(() => import("../pages/Home/Home"))
+const ProductPage = lazy(() => import("../pages/Product/ProductPage"))
+const LoginPage = lazy(() => import("../pages/LoginPage/LoginPage"))
+const CartPage = lazy(() => import("../pages/CartPage/CartPage"))
+const CheckoutPage = lazy(() => import("../pages/Checkout/CheckoutPage"))
+const ProfilePage = lazy(() => import("../pages/Profile/ProfilePage"))
+
 export default function RoutesApp() {
   return (
-    <Routes>
+    <Suspense fallback={null}>
+      <Routes>
 
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
 
-      <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
 
-      <Route
-        path="/login"
-        element={
-          <PublicRoute>
-            <LoginPage />
-          </PublicRoute>
-        }
-      />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
-      <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart" element={<CartPage />} />
 
-      <Route
-        path="/checkout"
-        element={
-          <CheckoutRoute>
-            <CheckoutPage />
-          </CheckoutRoute>
-        }
-      />
+        <Route
+          path="/checkout"
+          element={
+            <CheckoutRoute>
+              <CheckoutPage />
+            </CheckoutRoute>
+          }
+        />
 
-      <Route
-        path="/checkout/payment"
-        element={
-          <CheckoutRoute>
-            <CheckoutPage />
-          </CheckoutRoute>
-        }
-      />
+        <Route
+          path="/checkout/payment"
+          element={
+            <CheckoutRoute>
+              <CheckoutPage />
+            </CheckoutRoute>
+          }
+        />
 
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
 
-    </Routes>
+      </Routes>
+    </Suspense>
   )
 }
